7531ef968723d4c70b4fa1c83df63871ef2cef87
scriptspectra <scriptspectra@gmail.com>
2025-09-26 23:26:40 +0530

    howSum Recursion

---
+++ howSum DP/howSum Problem.py
@@ -0,0 +1,9 @@
+# howSum Problem:
+# Given a target sum and a list of numbers, return any combination of numbers
+# from the list that add up exactly to the target sum. 
+# You can use an element of the list as many times as needed.
+# If there is no combination that adds up to the target sum, return None.
+#
+# Example:
+# howSum(7, [5, 3, 4, 7]) -> [3, 4] or [7]
+# howSum(7, [2, 4])       -> None
---
+++ howSum DP/howSum_recursion.py
@@ -0,0 +1,12 @@
+def howSumRecursion(numbers, targetSum):
+    if targetSum == 0: return []
+    if targetSum < 0: return None
+
+    for number in numbers:
+        remainder = targetSum - number
+        result = howSumRecursion(numbers, remainder)
+        if result is not None:
+            return result + [number]
+        
+
+print(howSumRecursion([2,3,4,7], 7))
